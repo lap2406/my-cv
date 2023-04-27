16f0d61afdb925b466998431aa5fe65cf6bf0c56
lap2406 <lap24062000@gmail.com>
2023-04-27 07:42:47 +0700

    - new build, layout/cv: responsive

--- src/layout/cv/index.js
+++ src/layout/cv/index.js
@@ -58,7 +58,7 @@ const CV = () => {
 
   return (
     <div className="w-screen md:h-screen h-auto md:overflow-hidden snap-y flex cv bg-cover fixed top-0 left-0 right-0 bottom-0 backdrop-blur intro-x">
-      <div className="md:w-9/12 w-full md:h-4/6 h-auto p-2 md:p-0 md:absolute md:-translate-x-2/4 md:-translate-y-2/4 top-1/2 left-1/2 bg-white rounded-sm bg-opacity-60 shadow-md">
+      <div className="md:w-9/12 w-full md:h-4/6 h-auto p-2 md:p-0 md:absolute md:-translate-x-2/4 md:-translate-y-2/4 top-1/2 left-1/2 bg-white rounded-sm bg-opacity-60 shadow-md md:overflow-hidden overflow-auto">
         <div className="w-full md:h-full h-auto overflow-auto relative grid grid-cols-3">
           <div className="md:col-span-1 col-span-3 md:h-auto h-64">
             <Social />
